4a3a1733338b15015f267412e20cee97e75a7de7
zaKing1 <40494728+zaKing1@users.noreply.github.com>
2018-07-11 23:14:43 +0300

    Update robotzel.py

--- robotzel.py
+++ robotzel.py
@@ -351,17 +351,6 @@ async def debug(ctx, *args):
         await bot.say("You don't have permissions for this command.")
 
 
-Client = Bot('r!')
-
-@Client.command(pass_context = True)
-async def clear(ctx, number):
-    number = int(number) #Converting the amount of messages to delete to an integer
-    counter = 0
-    async for x in Client.logs_from(ctx.message.channel, limit = number):
-        if counter < number:
-            await Client.delete_message(x)
-            counter += 1
-            await asyncio.sleep(1.2) #1.2 second timer so the deleting process can be even
 
 
 
