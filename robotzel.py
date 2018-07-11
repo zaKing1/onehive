fd379b20d729b4e7f402a1cb48c0ba981606f8cb
zaKing1 <40494728+zaKing1@users.noreply.github.com>
2018-07-11 23:04:36 +0300

    Update robotzel.py

--- robotzel.py
+++ robotzel.py
@@ -350,13 +350,7 @@ async def debug(ctx, *args):
     else:
         await bot.say("You don't have permissions for this command.")
 
-@Client.command(pass_context = True)
-async def clear(ctx, number):
-    mgs = [] #Empty list to put all the messages in the log
-    number = int(number) #Converting the amount of messages to delete to an integer
-    async for x in Client.logs_from(ctx.message.channel, limit = number):
-        mgs.append(x)
-    await Client.delete_messages(mgs)
+
 
 
 bot.run(os.getenv("TOKEN"))
